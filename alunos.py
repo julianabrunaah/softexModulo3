707ec3780f3d77affce3519ac343e330eddad2c7
Juliana S <julianabrunahh@gmail.com>
2022-07-01 01:42:05 -0300

    Initial commit

---
+++ alunos.py
@@ -0,0 +1,27 @@
+nomes = ['Ana', 'Maria','Pedro','Lucas','João']
+repfalta = 3
+
+print('___AGENDA DE CLASSE___')
+for nome in nomes:
+    print(nome)
+    nota1 = float(input('Digite a primeira nota :'))
+    nota2 = float(input('Digite a segunda nota: '))
+    faltas = int(input('Digite a quantidade de faltas: '))
+    media = (nota1 + nota2)/2
+
+    if faltas>=3:
+        print(nome,' Reprovado por falta')
+    if media<7:
+        print(nome,' Reprovado por nota')
+    if media>=7 and faltas<3:
+        print(nome,' Aprovado/a')
+
+
+
+
+
+
+
+
+
+
