06d747f9583e7bd44c7b3c7d781910dc3aa9ea66
Juliana S <julianabrunahh@gmail.com>
2022-07-04 17:10:32 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -3,15 +3,15 @@ num1= int(input('Digite um valor de 0 a 10: '))
 num2= int(input('Digite um valor de 0 a 10: '))
 operador = str(input('digite e operação que deseja realizar''\n Exemplo: soma''\n digite a operação: '))
 
-def soma (num2, num1, resSoma):
+def soma (num2, num1, res):
     resSoma = num1 + num2
-    print(resSoma)
-    return resSoma
+    print(res)
+    return res
     
-def subtracao (num2, num1, resSub):
+def subtracao (num2, num1, res):
     resSub = num1 - num2
-    print(resSub)
-    return resSub
+    print(res)
+    return res
 
 def multi (num2, num1, res):
     res = num1 - num2
@@ -24,9 +24,9 @@ def divisao (num2, num1, res):
     return res
     
 if 'soma' in operador:
-  soma(num1, num2, resSoma)
+  soma(num1, num2, res)
 elif 'subtracao' in operador:
-  subtracao(num1, num2, resSub)
+  subtracao(num1, num2, res)
 elif 'multiplicacao' in operador:
   multi(num1, num2, res)
 elif 'divisao' in operador:
