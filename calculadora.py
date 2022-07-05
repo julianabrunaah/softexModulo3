856448035f24783ab1e2ba3b6cb1a243d69d4f21
Juliana S <julianabrunahh@gmail.com>
2022-07-05 12:33:47 -0300

    Update calculadora.py
    calculadora finalizada

--- calculadora.py
+++ calculadora.py
@@ -1,35 +1,42 @@
 
-num1= int(input('Digite um valor de 0 a 10: '))
-num2= int(input('Digite um valor de 0 a 10: '))
-operador = str(input('digite e operação que deseja realizar''\n Exemplo: soma''\n digite a operação: '))
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite um valor: '))
+print('Sabendo que: \n 1: Soma \n 2: Subtração \n 3: Multiplicação \n 4: Divisão')
+operador = str(input('Digite o valor correspondente a operação que deseja realizar: '))
 
-def soma (num2, num1, soma):
+
+
+def soma(num1, num2, soma):
     soma = num1 + num2
-    print(soma)
+    print('Resuldado =', soma)
     return soma
-    
-def subtracao (num2, num1, subtracao):
+
+
+def subtracao(num1, num2, subtracao):
     subtracao = num1 - num2
-    print(subtracao)
+    print('Resuldado =', subtracao)
     return subtracao
 
-def multiplicacao (num2, num1, multiplicacao):
+
+def multiplicacao(num1, num2, multiplicacao):
     multiplicacao = num1 * num2
-    print(multiplicacao)
+    print('Resuldado =', multiplicacao)
     return multiplicacao
-    
-def divisao (num2, num1, divisao):
-    divisao = num1/num2
-    print(divisao)
+
+
+def divisao(num1, num2, divisao):
+    divisao = num1 / num2
+    print('Resuldado =', divisao)
     return divisao
-    
-if 'soma' in operador:
-  soma(num1, num2, soma)
-elif 'subtracao' in operador:
-  subtracao(num1, num2, subtracao)
-elif 'multiplicacao' in operador:
-  multiplicacao(num1, num2, multiplicacao)
-elif 'divisao' in operador:
-  divisao(num1, num2, divisao)
+
+
+if '1' in operador:
+    soma(num1, num2, soma)
+elif '2' in operador:
+    subtracao(num1, num2, subtracao)
+elif '3' in operador:
+    multiplicacao(num1, num2, multiplicacao)
+elif '4' in operador:
+    divisao(num1, num2, divisao)
 else:
-  print('0')
+    print('0')
