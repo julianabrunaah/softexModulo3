37b5da02c640918eb54a5e6f6a5e5e43e0ecaa05
Juliana S <julianabrunahh@gmail.com>
2022-07-05 14:23:22 -0300

    Create anodenascimento.py

---
+++ anodenascimento.py
@@ -0,0 +1,20 @@
+
+nome = str(input('Digite seu nome: '))
+aniversario = True
+while aniversario:
+    ano = int(input('Digite o seu ano de nascimento: '))
+    try:
+        if ano>=1920 and ano<=2021:
+            atual = 2022
+            idade = atual - ano
+            print('Olá ', nome)
+            print('Sua idade é ', idade)
+            aniversario= False
+        # else incluido para corrigir erro
+        else:
+            print('Ano de nascimento inválido')
+    except:
+        # a mensagem
+        # print('Ano de nascimento inválido')
+        # por algum erro que não identifiquei não aparecia no terminal
+        continue
